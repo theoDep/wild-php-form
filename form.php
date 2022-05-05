2719f3538489d55e3ebf82b7707cf1e8800bc7a9
theoDep <depelsemackertheo@yahoo.fr>
2022-05-05 20:16:28 +0200

    Finished exercice

--- form.php
+++ form.php
@@ -1,33 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
   <form  action="/thanks.php"  method="post">
-    <div>
-      <label  for="nom">Nom :</label>
-      <input  type="text"  id="nom"  name="user_name">
-    </div>
-    <div>
-      <label  for="prenom">Prenom :</label>
-      <input  type="text"  id="prenom"  name="user_firstname">
-    </div>
-    <div>
-      <label  for="courriel">Courriel :</label>
-        <input  type="email"  id="courriel"  name="user_email">
-    </div>
-    <div>
-      <label  for="phone">tel :</label>
-      <input  type="text"  id="phone"  name="user_phone">
-    </div>
-    <div>
-      <label  for="subject">Sujet :</label>
-      <select  id="subjects"  name="user_subjects">
-        <option value="probleme">Probleme</option>
-        <option value="apres-vente">Service apres-vente</option>
-        <option value="livraison">Livraison</option>
-</select>
-    </div>
-    <div>
-      <label  for="message">Message :</label>
-      <textarea  id="message"  name="user_message"></textarea>
-    </div>
-    <div  class="button">
-      <button  type="submit">Envoyer votre message</button>
-    </div>
-  </form>
+      <div>
+        <label  for="nom">Nom :</label>
+        <input  type="text"  id="nom"  name="user_name">
+        <?php if(!empty($errors['user_name1'])){ echo $errors['user_name1']; } ?>
+      </div>
+      <div>
+        <label  for="prenom">Prenom :</label>
+        <input  type="text"  id="prenom"  name="user_firstname">
+        <?php if(!empty($errors['user_firstname'])){ echo $errors['user_firstname']; } ?>
+      </div>
+      <div>
+        <label  for="courriel">Courriel :</label>
+          <input  type="email"  id="courriel"  name="user_email">
+          <?php if(!empty($errors['user_email'])){ echo $errors['user_email']; } ?>
+      </div>
+      <div>
+        <label  for="phone">tel :</label>
+        <input  type="text"  id="phone"  name="user_phone">
+        <?php if(!empty($errors['user_phone'])){ echo $errors['user_phone']; } ?>
+      </div>
+      <div>
+        <label  for="subject">Sujet :</label>
+        <select  id="subjects"  name="user_subjects">
+          <option value="probleme">Probleme</option>
+          <option value="apres-vente">Service apres-vente</option>
+          <option value="livraison">Livraison</option>
+  </select>
+        <?php if(!empty($errors['user_subjects'])){ echo $errors['user_subjects']; } ?>
+      </div>
+      <div>
+        <label  for="message">Message :</label>
+        <textarea  id="message"  name="user_message"></textarea>
+        <?php if(!empty($errors['user_message'])){ echo $errors['user_message']; } ?>
+      </div>
+      <div  class="button">
+        <button  type="submit">Envoyer votre message</button>
+      </div>
+    </form>
+  
+</body>
+</html>
--- thanks.php
+++ thanks.php
@@ -8,9 +8,62 @@
  'user_subjects' => $user_subjects,
  'user_message' => $user_message] = $_POST;
 
-echo nl2br ("Merci $user_firstname $user_name de nous avoir contacté à propos de 
-\"$user_subjects\".\n
-\n
-Un de nos conseiller vous contactera soit à l’adresse $user_email ou par téléphone au $user_phone dans les plus brefs délais pour traiter votre demande : \n
-\n
+$errors = [];
+
+if(empty($_POST['user_name'])){
+    $errors['user_name1'] = '* Veuillez entrer votre nom';
+} else {
+  if (!preg_match("/^[a-zA-Z ]*$/",$_POST['user_name'])) {
+    $errors['user_name2'] = '* Veuillez entrer un nom valide';
+  }
+}
+
+if(empty($_POST['user_firstname'])){
+    $errors['user_firstname'] = '* Veuillez entrer votre prenom';
+} else {
+  if (!preg_match("/^[a-zA-Z ]*$/",$_POST['user_firstname'])) {
+    $errors['user_firstname'] = '* Veuillez entrer un prenom valide';
+  }
+}
+
+if(empty($_POST['user_email'])){
+    $errors['user_email'] = '* Veuillez entrer votre email';
+} else {
+  if (!filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)) {
+    $errors['user_email'] = '* Veuillez entrer un email valide';
+  }
+}
+
+if(empty($_POST['user_phone'])){
+    $errors['user_phone'] = '* Veuillez entrer votre numero de téléphone';
+} else {
+  if (!preg_match("/^[0-9]*$/",$_POST['user_phone'])) {
+    $errors['user_phone'] = '* Veuillez entrer un numero de téléphone valide';
+  }
+}
+
+if(empty($_POST['user_subjects'])){
+    $errors['user_subjects'] = '* Veuillez choisir votre sujet';
+} else {
+  if (!preg_match("/^[a-zA-Z ]*$/",$_POST['user_subjects'])) {
+    $errors['user_subjects'] = '* Veuillez entrer un sujet valide';
+  }
+}
+
+if(empty($_POST['user_message'])){
+    $errors['user_message'] = '* Veuillez entrer votre message';
+} else {
+  if (!preg_match("/^[a-zA-Z ]*$/",$_POST['user_message'])) {
+    $errors['user_message'] = '* Veuillez entrer un message valide';
+  }
+}
+
+if (!empty($errors)) {
+  foreach ($errors as $error) {
+    echo $error . '<br>';
+  }
+} else {
+echo nl2br ("Merci $user_firstname $user_name de nous avoir contacté à propos de \"$user_subjects\".\n
+Un de nos conseiller vous contactera soit à l’adresse $user_email ou par téléphone au $user_phone dans les plus brefs délais pour traiter votre demande :\n
 $user_message");
+}
